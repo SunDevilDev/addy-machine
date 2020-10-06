f0f96471f8d859680ff17f0b867c08dd727b2017
cas smith <csmit139@asu.edu>
2020-10-05 18:38:11 -0700

    Initial commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () { //theoritically gets the total value and returns it
+    return 0;
+  }
+  
+  public void add (int value) {//theoritically sets the total value to the result of the sum
+  }
+
+  public void subtract (int value) {//theoritically sets the total value to the result of the subtraction
+  }
+
+  public String toString () { //formats an output and prints it with the value
+    return "";
+  }
+
+  public void clear() { //deconstructor method, i assume
+  }
+}
